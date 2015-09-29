9cf3435b1677efb34f99f8e07544a2d22c893db6
Noe SIERRA-VELASQUEZ <nsierra-@e1r12p10.42.fr>
2015-09-29 17:04:25 +0200

    Quit ok

--- src/close_client_connection.c
+++ src/close_client_connection.c
@@ -1,6 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "server.h"
+#include "libft.h"
+
 int							close_client_connection(int fd, const char **input)
 {
-	(void)fd;
 	(void)input;
+	respond(fd, Success, "Bye bye.");
+	close(fd);
+	ft_putendl("Connection ended.");
+	exit(EXIT_SUCCESS);
 	return (1);
 }
