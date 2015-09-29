afad0787887a66da0698c59c06d601c0c1e5e46c
Noe SIERRA-VELASQUEZ <nsierra-@e1r12p10.42.fr>
2015-09-29 16:55:41 +0200

    ls ok

--- src/list_files.c
+++ src/list_files.c
@@ -1,6 +1,43 @@
+#include <stddef.h>
+#include <dirent.h>
+#include "server.h"
+#include "libft.h"
+
+static char					*build_file_list(DIR *directory)
+{
+	struct dirent			*entity;
+	char					*answer;
+	char					*tmp;
+
+	entity = NULL;
+	answer = ft_strnew(0);
+	while ((entity = readdir(directory)))
+	{
+		tmp = answer;
+		answer = ft_strjoin(answer, entity->d_name);
+		ft_strdel(&tmp);
+		tmp = answer;
+		answer = ft_strjoin(answer, "\n");
+		ft_strdel(&tmp);
+	}
+	return answer;
+}
+
 int							list_files(int fd, const char **input)
 {
-	(void)fd;
+	DIR						*directory;
+	char					*answer;
+
 	(void)input;
+	directory = NULL;
+	if (!(directory = opendir(".")))
+	{
+		respond(fd, Failure, "opendir() failed.");
+		return (0);
+	}
+	answer = build_file_list(directory);
+	closedir(directory);
+	respond(fd, Success, answer);
+	ft_strdel(&answer);
 	return (1);
 }
